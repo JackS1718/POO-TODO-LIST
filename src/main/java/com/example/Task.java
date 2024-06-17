94232ad06073a341ceb1689d8f31f4fccaef4ef4
NCorvusH <corvusfh@gmail.com>
2024-06-17 09:31:01 +0200

    Merging

---
+++ src/main/java/com/example/Task.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        String status = isCompleted ? "[X]" : "[ ]";
+        return status + " " + description;
+    }
+}
